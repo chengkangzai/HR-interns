622c54162de0eb3058ca6799c6c3a2008a5eda13
Ching Cheng Kang <pycck@hotmail.com>
2024-01-14 17:31:59 +0800

    downgrade php to php81 and use fall back route

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,6 @@
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    $candidate = App\Models\Candidate::find(1);
-    $email = App\Models\Email::find(1);
-
-    Mail::to($candidate->email)->send(new \App\Mail\DefaultMail($candidate, $email));
-
-    return view('welcome');
+Route::fallback(function () {
+    return redirect()->to(filament()->getUrl());
 });
