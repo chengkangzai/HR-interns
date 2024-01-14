380719c62f98106f49cd53301b7643bb94b23216
Ching Cheng Kang <pycck@hotmail.com>
2024-01-14 15:00:37 +0800

    wip

--- app/Enums/JobStatus.php
+++ app/Enums/JobStatus.php
@@ -2,8 +2,28 @@
 
 namespace App\Enums;
 
-enum JobStatus: string
+use Filament\Models\Contracts\HasName;
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasLabel;
+
+enum JobStatus: string implements HasLabel,HasColor
 {
     case OPEN = 'open';
     case CLOSED = 'closed';
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::OPEN => 'success',
+            self::CLOSED => 'danger',
+        };
+    }
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::OPEN => 'Open',
+            self::CLOSED => 'Closed',
+        };
+    }
 }
