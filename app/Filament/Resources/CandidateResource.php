4319f7aeae44e82ecd54f7f8a6b2b1f0c26d3fee
chengkangzai <chengkangzai@users.noreply.github.com>
2024-01-14 10:39:34 +0000

    Fix styling

--- app/Filament/Resources/CandidateResource.php
+++ app/Filament/Resources/CandidateResource.php
@@ -64,17 +64,18 @@ class CandidateResource extends Resource
             Section::make([
                 Select::make('position_id')
                     ->suffixAction(function (string $context, ?Candidate $record) {
-                        if (!$record) {
+                        if (! $record) {
                             return null;
                         }
                         if ($context == 'create') {
                             return null;
                         }
+
                         return FormAction::make('view_position')
                             ->icon('heroicon-o-eye')
                             ->url(PositionResource::getUrl('view', ['record' => $record->position_id]), true);
                     })
-                    ->relationship('position', 'title', fn(Builder $query) => $query->where('status', PositionStatus::OPEN))
+                    ->relationship('position', 'title', fn (Builder $query) => $query->where('status', PositionStatus::OPEN))
                     ->createOptionForm([
                         TextInput::make('title')
                             ->required(),
@@ -108,11 +109,11 @@ class CandidateResource extends Resource
 
             Placeholder::make('created_at')
                 ->label('Created Date')
-                ->content(fn(?Candidate $record): string => $record?->created_at?->diffForHumans() ?? '-'),
+                ->content(fn (?Candidate $record): string => $record?->created_at?->diffForHumans() ?? '-'),
 
             Placeholder::make('updated_at')
                 ->label('Last Modified Date')
-                ->content(fn(?Candidate $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
+                ->content(fn (?Candidate $record): string => $record?->updated_at?->diffForHumans() ?? '-'),
 
         ]);
     }
@@ -136,8 +137,8 @@ class CandidateResource extends Resource
 
                 TextColumn::make('range')
                     ->label('From - To')
-                    ->getStateUsing(fn(Candidate $record) => isset($record->from, $record->to)
-                        ? $record->from->format('d/m/Y') . ' - ' . $record->to->format('d/m/Y')
+                    ->getStateUsing(fn (Candidate $record) => isset($record->from, $record->to)
+                        ? $record->from->format('d/m/Y').' - '.$record->to->format('d/m/Y')
                         : 'N/A'
                     ),
 
@@ -165,7 +166,7 @@ class CandidateResource extends Resource
                         Select::make('status')
                             ->options(CandidateStatus::class),
                     ])
-                    ->action(fn(Candidate $record, array $data) => $record->update($data)),
+                    ->action(fn (Candidate $record, array $data) => $record->update($data)),
                 EditAction::make(),
                 DeleteAction::make(),
             ])
@@ -174,7 +175,7 @@ class CandidateResource extends Resource
                 BulkAction::make('send_email')
                     ->form([
                         Select::make('email')
-                            ->options(fn() => Email::pluck('name', 'id')),
+                            ->options(fn () => Email::pluck('name', 'id')),
                     ])
                     ->action(function (Collection $records, array $data) {
                         $email = Email::find($data['email']);
@@ -188,7 +189,7 @@ class CandidateResource extends Resource
                             ->options(CandidateStatus::class),
                     ])
                     ->action(function (Collection $records, array $data) {
-                        $records->each(fn(Candidate $record) => $record->update([
+                        $records->each(fn (Candidate $record) => $record->update([
                             'status' => $data['status'],
                         ]));
                     }),
