58894d346fdc537f2c1cc3592c489d4907136aaf
Ching Cheng Kang <pycck@hotmail.com>
2024-01-14 14:03:03 +0800

    show tab

--- app/Filament/Resources/CandidateResource/Pages/ListCandidates.php
+++ app/Filament/Resources/CandidateResource/Pages/ListCandidates.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\CandidateResource\Pages;
 
+use App\Enums\CandidateStatus;
 use App\Filament\Resources\CandidateResource;
+use App\Models\Candidate;
 use Filament\Actions\CreateAction;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListCandidates extends ListRecords
 {
@@ -14,6 +18,34 @@ class ListCandidates extends ListRecords
     {
         return [
             CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All')
+                ->badge(Candidate::count()),
+            'pending' => Tab::make('Pending')
+                ->badgeColor(CandidateStatus::PENDING->getColor())
+                ->badge(Candidate::where('status', CandidateStatus::PENDING)->count())
+                ->query(fn(Builder $query) => $query->where('status', CandidateStatus::PENDING)),
+            'technical_test' => Tab::make('Technical Test')
+                ->badgeColor(CandidateStatus::TECHNICAL_TEST->getColor())
+                ->badge(Candidate::where('status', CandidateStatus::TECHNICAL_TEST)->count())
+                ->query(fn(Builder $query) => $query->where('status', CandidateStatus::TECHNICAL_TEST)),
+            'interview' => Tab::make('Interview')
+                ->badgeColor(CandidateStatus::INTERVIEW->getColor())
+                ->badge(Candidate::where('status', CandidateStatus::INTERVIEW)->count())
+                ->query(fn(Builder $query) => $query->where('status', CandidateStatus::INTERVIEW)),
+            'expired' => Tab::make('Expired')
+                ->badgeColor(CandidateStatus::EXPIRED->getColor())
+                ->badge(Candidate::where('status', CandidateStatus::EXPIRED)->count())
+                ->query(fn(Builder $query) => $query->where('status', CandidateStatus::EXPIRED)),
+            'withdrawn' => Tab::make('Withdrawn')
+                ->badgeColor(CandidateStatus::WITHDRAWN->getColor())
+                ->badge(Candidate::where('status', CandidateStatus::WITHDRAWN)->count())
+                ->query(fn(Builder $query) => $query->where('status', CandidateStatus::WITHDRAWN)),
         ];
     }
 }
