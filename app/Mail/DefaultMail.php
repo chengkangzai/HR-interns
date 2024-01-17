70578601a53c9c87c4d6ee63255642aeda6122b7
chengkangzai <chengkangzai@users.noreply.github.com>
2024-01-17 06:11:14 +0000

    Fix styling

--- app/Mail/DefaultMail.php
+++ app/Mail/DefaultMail.php
@@ -34,7 +34,7 @@ class DefaultMail extends Mailable implements ShouldQueue
             from: config('mail.from.address'),
             to: [$this->candidate->email],
             cc: [
-                $this->email->cc
+                $this->email->cc,
             ],
             subject: $this->email->title
         );
