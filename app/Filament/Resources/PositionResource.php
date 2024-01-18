2370fad8a5adecaaf954e5455ec73ecbcd4b0a10
chengkangzai <chengkangzai@users.noreply.github.com>
2024-01-18 06:47:44 +0000

    Fix styling

--- app/Filament/Resources/PositionResource.php
+++ app/Filament/Resources/PositionResource.php
@@ -9,7 +9,6 @@ use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
