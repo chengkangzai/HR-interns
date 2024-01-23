bd95eb51e0d04d978d4c76955119ed8102414882
Ching Cheng Kang <pycck@hotmail.com>
2024-01-23 14:26:14 +0800

    update status

--- app/Enums/CandidateStatus.php
+++ app/Enums/CandidateStatus.php
@@ -8,24 +8,28 @@ use Filament\Support\Contracts\HasLabel;
 
 enum CandidateStatus: string implements HasColor, HasLabel
 {
-    case PENDING = 'pending'; // Indicates the candidate has been sent an invitation
+    case PENDING = 'pending'; // Indicates the candidate has been recorded but not contacted
+    case CONTACTED = 'contacted'; // Indicates the candidate has been contacted
     case TECHNICAL_TEST = 'technical_test'; // Indicates the candidate has been sent a technical test
     case INTERVIEW = 'interview'; // Indicates the candidate has been interviewed
     case WITHDRAWN = 'withdrawn'; // Indicates the candidate has withdrawn from the process
     case HIRED = 'hired'; // Indicates the candidate has accepted the offer and has started
     case OFFER_ACCEPTED = 'offer_accepted'; // Indicates the candidate has accepted the offer but has not yet started
     case COMPLETED = 'completed'; // Indicates the candidate has completed internship
+    case NO_RESPONSE = 'no_response'; // Indicates the candidate has not responded to the invitation
 
     public function getLabel(): ?string
     {
         return match ($this) {
             self::PENDING => 'Pending',
+            self::CONTACTED => 'Contacted',
             self::TECHNICAL_TEST => 'Technical Test',
             self::INTERVIEW => 'Interview',
             self::WITHDRAWN => 'Withdrawn',
             self::HIRED => 'Hired',
             self::OFFER_ACCEPTED => 'Offer Accepted',
             self::COMPLETED => 'Completed',
+            self::NO_RESPONSE => 'No Response',
         };
     }
 
@@ -33,12 +37,14 @@ enum CandidateStatus: string implements HasColor, HasLabel
     {
         return match ($this) {
             self::PENDING => Color::Gray,
+            self::CONTACTED => Color::Indigo,
             self::TECHNICAL_TEST => Color::Blue,
             self::INTERVIEW => Color::Yellow,
             self::WITHDRAWN => Color::Orange,
             self::HIRED => Color::Lime,
             self::OFFER_ACCEPTED => Color::Sky,
             self::COMPLETED => Color::Green,
+            self::NO_RESPONSE => Color::Red,
         };
     }
 }
