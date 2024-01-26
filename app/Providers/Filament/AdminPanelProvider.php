1b9c8a59ffb8920bcd30344f45b15129b840f34d
Ching Cheng Kang <pycck@hotmail.com>
2024-01-26 16:48:46 +0800

    show logger

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,6 +54,9 @@ class AdminPanelProvider extends PanelProvider
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
             ])
+            ->resources([
+                config('filament-logger.activity_resource')
+            ])
             ->authMiddleware([
                 Authenticate::class,
             ]);
