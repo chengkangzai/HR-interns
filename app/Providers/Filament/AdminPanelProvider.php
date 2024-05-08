42dee726af9259c3f8644134bba0205c44a9361f
Ching Cheng Kang <pycck@hotmail.com>
2024-05-08 21:55:10 +0800

    introduce job monitor

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Croustibat\FilamentJobsMonitor\FilamentJobsMonitorPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -35,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Amber,
             ])
             ->plugins([
+                FilamentJobsMonitorPlugin::make(),
                 FilamentFullCalendarPlugin::make()
                     ->config([
                         'headerToolbar' => [
