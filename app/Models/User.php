634c2581bbb94909b4dc7b9ab264dc5da1ad20ef
Ching Cheng Kang <pycck@hotmail.com>
2024-05-08 21:53:54 +0800

    improve ux

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,10 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
+        if (app()->isLocal()) {
+            return true;
+        }
+
         return str_ends_with($this->email, '@pixalink.io') && $this->hasVerifiedEmail();
     }
 }
