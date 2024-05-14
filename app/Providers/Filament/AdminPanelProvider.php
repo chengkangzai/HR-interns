7e1e745fd1be864e48885eceb964b88912be09de
Ching Cheng Kang <pycck@hotmail.com>
2024-05-14 11:51:15 +0800

    improve ux

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->sidebarCollapsibleOnDesktop()
             ->plugins([
                 FilamentJobsMonitorPlugin::make(),
                 FilamentFullCalendarPlugin::make()
