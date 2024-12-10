7be38f7cdfd0fd3b9a44d192fc9f26c03113701a
chengkangzai <pycck@hotmail.com>
2024-12-10 11:38:11 +0800

    feat(app/Enums/PositionType.php): add label support to PositionType enum docs(app/Enums/PositionType.php): update enum documentation with new label functionality

--- app/Enums/PositionType.php
+++ app/Enums/PositionType.php
@@ -2,10 +2,22 @@
 
 namespace App\Enums;
 
-enum PositionType: string
+use Filament\Support\Contracts\HasLabel;
+
+enum PositionType: string implements HasLabel
 {
     case INTERN = 'intern';
     case CONTRACT = 'contract';
     case FULL_TIME = 'full_time';
     case PART_TIME = 'part_time';
+
+    public function getLabel(): ?string
+    {
+        return match ($this) {
+            self::INTERN => 'Internship',
+            self::CONTRACT => 'Contract Position',
+            self::FULL_TIME => 'Full-Time Position',
+            self::PART_TIME => 'Part-Time Position',
+        };
+    }
 }
