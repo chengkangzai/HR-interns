3ece66844aff41d718410dc8f71654b63d960cfb
chengkangzai <pycck@hotmail.com>
2024-12-16 13:44:22 +0800

    fix(positiontype.php): refactor PositionType enum to use HasColor trait and update color values feat(positiontype.php): add support for HasColor trait in PositionType enum, providing a getColor method with updated color values

--- app/Enums/PositionType.php
+++ app/Enums/PositionType.php
@@ -2,9 +2,11 @@
 
 namespace App\Enums;
 
+use Filament\Support\Colors\Color;
+use Filament\Support\Contracts\HasColor;
 use Filament\Support\Contracts\HasLabel;
 
-enum PositionType: string implements HasLabel
+enum PositionType: string implements HasColor, HasLabel
 {
     case INTERN = 'intern';
     case CONTRACT = 'contract';
@@ -18,6 +20,16 @@ enum PositionType: string implements HasLabel
             self::CONTRACT => 'Contract Position',
             self::FULL_TIME => 'Full-Time Position',
             self::PART_TIME => 'Part-Time Position',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::INTERN => Color::Cyan,
+            self::CONTRACT => Color::Lime,
+            self::FULL_TIME => Color::Violet,
+            self::PART_TIME => Color::Fuchsia,
         };
     }
 }
