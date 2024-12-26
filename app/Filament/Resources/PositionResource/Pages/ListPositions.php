8312544a6d6c882bca139d56cc1469bc54bd6d9b
chengkangzai <pycck@hotmail.com>
2024-12-26 12:00:58 +0800

    improve ux

--- app/Filament/Resources/PositionResource/Pages/ListPositions.php
+++ app/Filament/Resources/PositionResource/Pages/ListPositions.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Resources\PositionResource\Pages;
 
+use App\Enums\PositionType;
 use App\Filament\Resources\PositionResource;
+use App\Models\Position;
 use Filament\Actions\CreateAction;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListPositions extends ListRecords
 {
@@ -15,5 +19,24 @@ class ListPositions extends ListRecords
         return [
             CreateAction::make(),
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $positions = PositionType::cases();
+        $tabs = [
+            'all' => Tab::make(),
+        ];
+        foreach ($positions as $position) {
+            $count = Position::where('type', $position->value)->count();
+            if ($count >= 1) {
+                $tabs[$position->value] = Tab::make()
+                    ->badgeColor($position->getColor())
+                    ->badge($count)
+                    ->modifyQueryUsing(fn (Builder $query) => $query->where('type', $position->value));
+            }
+        }
+
+        return $tabs;
     }
 }
