b5a3b1202abefc4e1b91db4a5f9c6e1cb472331a
chengkangzai <pycck@hotmail.com>
2025-01-13 23:01:45 +0800

    update test data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'CCK',
-            'email' => 'cheng.kang@pixalink.io',
+            'name' => 'Example User',
+            'email' => 'example@example.io',
         ]);
     }
 }
