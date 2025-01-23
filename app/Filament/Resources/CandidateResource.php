fed4511c7b19e3eafc8340978e285be2a86b16e1
chengkangzai <pycck@hotmail.com>
2025-01-24 01:13:05 +0800

    ✨ (CandidateResource.php): replace Textarea with RichEditor for 'responsibilities' field to enhance content formatting capabilities

--- app/Filament/Resources/CandidateResource.php
+++ app/Filament/Resources/CandidateResource.php
@@ -29,7 +29,6 @@ use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\SpatieMediaLibraryFileUpload;
 use Filament\Forms\Components\SpatieTagsInput;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
@@ -429,10 +428,8 @@ class CandidateResource extends Resource
                                 }
                             }),
 
-                        Textarea::make('responsibilities')
+                        RichEditor::make('responsibilities')
                             ->columnSpanFull()
-                            ->rows(3)
-                            ->autosize()
                             ->placeholder('Describe your key responsibilities and achievements'),
 
                         Fieldset::make('duration')
